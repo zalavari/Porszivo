61f2404edea9afc92576380cca1cac4e89d31c85
kelemendomonkos <kelemendomonkos@gmail.com>
2018-10-14 20:10:43 +0200

    Added comments

--- Porszivo/Porszivo/DepthFirstSearchPathChooserAlgorithm.cs
+++ Porszivo/Porszivo/DepthFirstSearchPathChooserAlgorithm.cs
@@ -11,6 +11,7 @@ namespace Porszivo
     {
         readonly Robot robot;
 
+        //Itt tárolja, hogy merre lépett
         Stack<Direction> directionStack = new Stack<Direction>();
 
         public DepthFirstSearchPathChooserAlgorithm(Robot robot)
@@ -21,7 +22,8 @@ namespace Porszivo
         public Direction move()
         {
             Direction direction = Direction.STAY;
-            
+
+            //Végignézi Fel-Balra-Le-Jobbra prioritással, hogy merre tud lépni (ha egyik irányba se akkor marad)
             if (isDirty(robot.positionX, robot.positionY - 1))
                 direction = Direction.UP;
             else if (isDirty(robot.positionX - 1, robot.positionY))
@@ -31,9 +33,10 @@ namespace Porszivo
             else if (isDirty(robot.positionX + 1, robot.positionY))
                 direction = Direction.RIGHT;
 
+            //Ha nem tudott lépni egyik irányba sem (és már elindult, nem a sensorokra vár) akkor visszamegy arra amerről jött
             if (direction == Direction.STAY && directionStack.Count != 0)
                 direction = reverseDirection(directionStack.Pop());
-            else
+            else     //Ha tudott lépni akkor feljegyzi, hogy merre
                 directionStack.Push(direction);
 
             return direction;
